cbd1468c1b18e293e25e5283c8ca6def0b111aee
kostyamospan <kostya04042001@gmail.com>
2022-05-04 19:13:44 +0300

    get validator fix

--- validator-node/run.sh
+++ validator-node/run.sh
@@ -4,7 +4,7 @@ mkdir -p "./geth" && cp ../static-nodes.json ./geth/
 
 echo "Static nodes file is copied to ./geth";
 
-VALIDATOR_ADDRESS=$(echo './validator')
+VALIDATOR_ADDRESS=$(echo ./validator)
 CHAIN_ID=$(../get-chain-id.sh ../genesis.json)
 
 echo "VALIDATOR IS $VALIDATOR_ADDRESS"
