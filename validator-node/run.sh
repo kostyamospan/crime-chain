5e0140d3e1e308fb2c6406eb15801badbe0bcd50
kostyamospan <kostya04042001@gmail.com>
2022-05-04 19:03:58 +0300

    chain id fix

--- validator-node/run.sh
+++ validator-node/run.sh
@@ -4,8 +4,8 @@ mkdir -p "./geth" && cp ../static-nodes.json ./geth/
 
 echo "Static nodes file is copied to ./geth";
 
-VALIDATOR_ADDRESS=echo "./validator"
-CHAIN_ID= echo ../get-chain-id.sh
+VALIDATOR_ADDRESS=$(echo './validator')
+CHAIN_ID=$(../get-chain-id.sh)
 
 geth    --networkid $CHAIN_ID \
        	--datadir ./ \
