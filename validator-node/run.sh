e8051390b8d8639a6cbade57c032ffa5989410da
kostyamospan <kostya04042001@gmail.com>
2022-05-04 19:13:13 +0300

    validator address echo

--- validator-node/run.sh
+++ validator-node/run.sh
@@ -7,7 +7,7 @@ echo "Static nodes file is copied to ./geth";
 VALIDATOR_ADDRESS=$(echo './validator')
 CHAIN_ID=$(../get-chain-id.sh ../genesis.json)
 
-echo "CHAIN ID IS $CHAIN_ID"
+echo "VALIDATOR IS $VALIDATOR_ADDRESS"
 
 geth    --networkid $CHAIN_ID \
        	--datadir ./ \
