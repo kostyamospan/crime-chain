a43fe81e15e38f6e64178f99b356c7ccfde041ab
kostyamospan <kostya04042001@gmail.com>
2022-05-04 19:16:16 +0300

    get pasword fix

--- validator-node/run.sh
+++ validator-node/run.sh
@@ -5,6 +5,7 @@ mkdir -p "./geth" && cp ../static-nodes.json ./geth/
 echo "Static nodes file is copied to ./geth";
 
 VALIDATOR_ADDRESS=$(cat ./validator)
+PASSWORD=$(cat ./password)
 CHAIN_ID=$(../get-chain-id.sh ../genesis.json)
 
 echo "VALIDATOR IS $VALIDATOR_ADDRESS"
@@ -13,5 +14,5 @@ geth    --networkid $CHAIN_ID \
        	--datadir ./ \
        	--unlock $VALIDATOR_ADDRESS \
        	--mine \
-		--password ./password \ 
+		--password $PASSWORD \ 
 		--port 30311 
