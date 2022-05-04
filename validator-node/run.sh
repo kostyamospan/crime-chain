80583d3b9fd7f698a90ea59fdd3248a81c5dc68e
kostyamospan <kostya04042001@gmail.com>
2022-05-04 19:00:42 +0300

    validator echo

--- validator-node/run.sh
+++ validator-node/run.sh
@@ -5,11 +5,11 @@ mkdir -p "./geth" && cp ../static-nodes.json ./geth/
 echo "Static nodes file is copied to ./geth";
 
 VALIDATOR_ADDRESS=echo "./validator"
-CHAIN_ID=../get-chain-id.sh
+CHAIN_ID= echo ../get-chain-id.sh
 
 geth    --networkid $CHAIN_ID \
        	--datadir ./ \
-       	--unlock ./validator \
+       	--unlock $VALIDATOR_ADDRESS \
        	--mine \
 		--password ./password \ 
 		--port 30311 
